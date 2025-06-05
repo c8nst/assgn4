6aa226a0a976a11e87cb67c4093606d273a31395
c8nst <kostiaferg@gmail.com>
2025-06-05 23:11:33 +0400

    this was ignored for some reason

--- client/ap/src/App.tsx
+++ client/ap/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('http://localhost:3000');
 
 interface PlayerState {
   paddleY: number;
